dae0ddf4e134b8a5697db334ed3ce9775ff19e00
backsani <118903562+backsani@users.noreply.github.com>
2023-11-14 13:56:53 +0900

    11/13 몬스터 움직임 구현
    CanAttack 마무리해야됨

--- Iron_Suit/Assets/Script/MonsterBasic.cs
+++ Iron_Suit/Assets/Script/MonsterBasic.cs
@@ -2,31 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class MonsterBasic : MonoBehaviour
 {
     PlayerController player;
-    CircleCollider2D colliderMonster;
+    CircleCollider2D attackCollider;
 
     [SerializeField] private float moveSpeed;
     [SerializeField] private float attackSpeed;
-    private bool CanMove;
+    private bool CanMove = true;
+    private bool CanAttack = false;
 
     // Start is called before the first frame update
     void Start()
     {
         player = FindObjectOfType<PlayerController>();
-        colliderMonster = GetComponentInChildren<"Range">
+        Transform childTransform = transform.Find("Range");
+        attackCollider = childTransform.GetComponent<CircleCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (CanMove)
-        {
-            Move();
-            return;
-        }
-        Attack();
+        if (CanMove) Move();
+        else if (CanAttack) Attack();
+
     }
 
     public virtual void Move()
@@ -37,18 +37,33 @@ public class MonsterBasic : MonoBehaviour
     public virtual void Attack()
     {
         float cooltime = attackSpeed;
-        cooltime -= Time.deltaTime;
-        if(cooltime <= 0)
+        if (CanAttack)
         {
-
+            Debug.Log("공격중");            
+            cooltime -= Time.deltaTime;
+            if (cooltime <= 0)
+            {
+                attackCollider.enabled = true;
+                Debug.Log("공격");
+            }
+            else if(cooltime <= -1)
+            {
+                Debug.Log("공격끝");
+                CanAttack = false;
+                attackCollider.enabled = true;
+            }
         }
+        else CanMove = true;
+
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.collider.CompareTag("Player"))
         {
+            Debug.Log("충돌");
             CanMove = false;
+            CanAttack = true;
         }
     }
 }
